0f23fba8b0e805a6f0cd4073a01bf03a9f53671e
zhanglp <zlp_15229380174@163.com>
2022-06-25 18:53:18 +0800

    优化

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import QuickUi from '../dist/quick-vue3-ui.es.js'
-console.log('QuickUi',QuickUi)
+// import QuickUi from '../dist/quick-vue3-ui.es.js'
+// console.log('QuickUi',QuickUi)
 const app=createApp(App)
-app.use(QuickUi)
+// app.use(QuickUi)
 app.mount('#app')
